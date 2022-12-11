3f1213f476a890b75d5e5bc4ea0ed1cbeba900ae
Ninad Bhangui <b.ninad50@gmail.com>
2022-12-11 21:10:39 +0530

    day03

---
+++ golang/day03/day.go
@@ -0,0 +1,86 @@
+package day01
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Move int
+type Outcome int
+
+// Enum representation in go
+const (
+	ROCK    Move = 1
+	PAPER   Move = 2
+	SCISSOR Move = 3
+)
+
+const (
+	LOSE Outcome = 0
+	DRAW Outcome = 3
+	WIN  Outcome = 6
+)
+
+func Calc1(inputFilepath string) int {
+	file, err := os.Open(inputFilepath)
+	defer file.Close()
+	if err != nil {
+		log.Fatal()
+	}
+	scanner := bufio.NewScanner(file)
+	var totalPriority int
+	for scanner.Scan() {
+		line := scanner.Text()
+		part1 := line[0 : len(line)/2]
+		part2 := line[len(line)/2:]
+		commonElements := findCommonElements(part1, part2)
+		totalPriority += findPriorityFromRune(commonElements[0])
+	}
+	return totalPriority
+}
+
+func Calc2(inputFilepath string) int {
+	file, err := os.Open(inputFilepath)
+	defer file.Close()
+	if err != nil {
+		log.Fatal()
+	}
+	scanner := bufio.NewScanner(file)
+	var totalPriority int
+	var group []string
+	var index int
+	for scanner.Scan() {
+		line := scanner.Text()
+		group = append(group, line)
+		if (index+1)%3 == 0 {
+			commonElementsFirst := findCommonElements(group[0], group[1])
+			commonElements := findCommonElements(group[2], string(commonElementsFirst))
+			totalPriority += findPriorityFromRune(commonElements[0])
+			group = nil
+		}
+		index++
+	}
+	return totalPriority
+}
+
+func findCommonElements(list1, list2 string) []rune {
+	var runes []rune
+	for _, e := range list1 {
+		if strings.ContainsRune(list2, e) {
+			runes = append(runes, e)
+		}
+	}
+	return runes
+}
+func findPriorityFromRune(character rune) int {
+	priority, _ := strconv.Atoi(fmt.Sprintf("%d", character))
+	if priority >= 97 && priority <= 122 {
+		return priority - 96
+	} else {
+		return priority - 64 + 26
+	}
+}
---
+++ golang/day03/day_test.go
@@ -0,0 +1,30 @@
+package day01
+
+import (
+	"fmt"
+	"testing"
+)
+
+var sampleFilePath = "inputs/sample1.txt"
+var inputFilePath = "inputs/input1.txt"
+
+func assertIntEquals(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected %d but got %d", want, got)
+	}
+}
+func TestFirst(t *testing.T) {
+	want := 157
+
+	got := Calc1(sampleFilePath)
+	assertIntEquals(t, got, want)
+	fmt.Println("day01: ", Calc1(inputFilePath))
+}
+
+func TestSecond(t *testing.T) {
+	want := 70
+	got := Calc2(sampleFilePath)
+	assertIntEquals(t, got, want)
+	fmt.Println("day02: ", Calc2(inputFilePath))
+}
